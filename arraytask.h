ae35edfb56d102c38fbb8be0dfec86f4b5e1d79e
ostap291 <135895825+ostap291@users.noreply.github.com>
2026-03-27 08:44:15 +0200

    Add files via upload

---
+++ arraytask.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include "json.hpp"
+#include <cassert>
+
+using namespace std;
+using json = nlohmann::json;
+
+class ArrayTask {
+public:
+    void run();
+
+    // Bubble Sort
+    void sortAsc(vector<int>& arr);  // ascending
+    void sortDesc(vector<int>& arr); // descending
+
+private:
+    void saveToJson(int S2, int S3, int duplicatesCount, const vector<int>& V1, const vector<int>& V2);
+    void printVector(const vector<int>& vec, const string& name);
+
+};
